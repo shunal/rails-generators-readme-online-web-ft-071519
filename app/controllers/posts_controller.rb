45750c84274fd515353c2f6cc418db80979b2568
Steven Hunal <stevehunal@gmail.com>
2019-10-08 01:24:57 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,10 +18,10 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(params.require(:post))
-    redirect_to post_path(@post)
-  end
+  @post = Post.find(params[:id])
+  @post.update(post_params(:title))
+  redirect_to post_path(@post)
+end
 
   def edit
     @post = Post.find(params[:id])
